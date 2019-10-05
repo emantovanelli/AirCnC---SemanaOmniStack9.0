3943b7b048ab2872ff85f630f5979a8e9499acf1
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-10-05 15:30:58 -0300

    Creating and listing spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
-
-// req.query = acessar os query params (filtro)
-// req.params = acessar route params (edição e delete)
-// req.body = acessar o corpo da mensagem (criacao e edicao)
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
