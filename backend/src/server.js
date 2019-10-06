c5f6c1b4076241dee2f0bcc482ac9cbc5974c570
Edvaldo Mantovanelli <ecmantovanelli@gmail.com>
2019-10-06 11:21:46 -0300

    showing spots created by the logged user

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@onminstack-8jdpp.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
